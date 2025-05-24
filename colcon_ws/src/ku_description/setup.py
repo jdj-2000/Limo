f8f601688a368a334de100f45ad4513deb3d36c6
jang daejin <jangdj2000@naver.com>
2025-05-24 10:07:28 +0900

    테스트

---
+++ colcon_ws/src/ku_description/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = 'ku_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            "share/" + package_name + "/launch",
+            glob(os.path.join("launch", "*.launch.py")),
+        ),
+        (
+            "share/" + package_name + "/urdf",
+            glob(os.path.join("urdf", "*.urdf")),
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jdj',
+    maintainer_email='jangdj2000@naver.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
